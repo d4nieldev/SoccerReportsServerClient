7dd3595ad6ad048356be38222a2ae6aa2768b816
spl211 <shembel@post.bgu.ac.il>
2023-01-19 20:33:26 +0200

    fixed server

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -5,17 +5,30 @@ import bgu.spl.net.srv.Server;
 public class StompServer {
 
     public static void main(String[] args) {
-        Server.stompThreadPerClient(
-                7777, //port
-                () -> new StompMessegingProtocolImpl(), //protocol factory
-                StompMessageEncoderDecoder::new //message encoder decoder factory
-        ).serve();
+        if(args.length < 2 ){
+            //Not legal
+            throw new IllegalArgumentException("Not legal arguments, Please insert port and server type.");
+        }
+        if(args[1].equals("reactor")){
+            System.out.println("Reactor server chosen!");
+            Server.stompThreadPerClient(
+                    Short.valueOf(args[0]), //port
+                    () -> new StompMessegingProtocolImpl(), //protocol factory
+                    StompMessageEncoderDecoder::new //message encoder decoder factory
+            ).serve();
+        }
 
-        // Server.stompReactor(
-        //         Runtime.getRuntime().availableProcessors(),
-        //         7777, //port
-        //         () -> new StompMessegingProtocolImpl(), //protocol factory
-        //         StompMessageEncoderDecoder::new //message encoder decoder factory
-        // ).serve();
+        else if(args[1].equals("tpc")){
+            System.out.println("TPC server chosen!");
+            Server.stompReactor(
+                    Runtime.getRuntime().availableProcessors(),
+                    Short.valueOf(args[0]), //port
+                    () -> new StompMessegingProtocolImpl(), //protocol factory
+                    StompMessageEncoderDecoder::new //message encoder decoder factory
+            ).serve();
+        }
+        else{
+            throw new IllegalArgumentException("Not legal server type, Please insert 'reactor' or 'tpc'.");
+        }
     }
 }
